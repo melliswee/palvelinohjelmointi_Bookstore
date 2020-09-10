658e33cbf9d4521a75eff893761097b65973c1e8
Melissa W <melissa.westberg@myy.haaga-helia.fi>
2020-09-10 16:10:56 +0300

    lisättiin thymeleaf-dependency pom:iin ja index.html templateen

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,14 +1,17 @@
 package hh.swd20.Bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
 	
 	@RequestMapping("/index")
-	public String HelloBooks() {
-		return "Hello books!"; //Tästä on hyvä jatkaa!
+	public String HelloBooks(Model model) {
+		String msg = "Hello books!";
+		model.addAttribute("message",msg);
+		return "index";
 	}
 
 }
