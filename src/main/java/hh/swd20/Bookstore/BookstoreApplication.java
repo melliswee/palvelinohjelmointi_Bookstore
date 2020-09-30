0071bfeb8482bc8fd71262a4114515e545e2a733
Melissa W <melissa.westberg@myy.haaga-helia.fi>
2020-09-30 13:31:14 +0300

    lisätty Category ja CategoryRepository

--- src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
@@ -1,5 +1,8 @@
 package hh.swd20.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
+import hh.swd20.Bookstore.domain.Category;
+import hh.swd20.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -17,15 +22,31 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository bookrepository) {
+	public CommandLineRunner demo(BookRepository bookrepository, CategoryRepository categoryrepository) {
 	return (args) -> {
 	// Your code...add some demo data to db
-		//Auto auto1 = new Auto("Tesla", 2016);
-		//Auto auto2 = new Auto("Saab", 1980);
+
 		Book book1 = new Book("Hitchhiker's guide to the Galaxy", "Douglas Adams", 1980, "978-051-7-54209-5", 35.00);
 		Book book2 = new Book("Merenpeitto", "Olli Jalonen", 2019, "978-951-1-34512-1", 19.95);
 		bookrepository.save(book1);
 		bookrepository.save(book2);
+		
+		Category cat1 = new Category("Scifi");
+		Category cat2 = new Category("Fantasy");
+		Category cat3 = new Category("Historical fiction");
+		
+		categoryrepository.save(cat1);
+		categoryrepository.save(cat2);
+		categoryrepository.save(cat3);
+		
+		/* log:ia ei saada ratkaistua vaikka on importit?
+		 * log.info("listataan kaikki kirjat");
+
+		for(Book book : bookrepository.findAll()) {
+
+			log.info(book.toString());
+
+		}*/
 		
 	};
 	}
